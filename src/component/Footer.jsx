37af0b918ecbf19da2f732cc6683a8bceb1364b2
Op0778 <prakashlkom5@gmail.com>
2025-09-19 16:53:11 +0530

    first

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../style/footerStyle.css";
 
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <>
+      <div className="foot">This Developed By Omprakash @ 2025</div>
+    </>
+  );
 };
 
 export default Footer;
