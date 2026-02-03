d5b58d98fcc3277cd7def6365223b1f1a1943a0f
Op0778 <prakashlkom5@gmail.com>
2026-02-03 19:52:17 +0530

    bugs solved

--- src/backend/server.js
+++ src/backend/server.js
@@ -13,6 +13,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// process.env.MONGO_URI
 mongoose
   .connect("process.env.MONGO_URI")
   .then(() => console.log(" MongoDB connected"))
@@ -558,8 +560,9 @@ app.delete(
     }
   },
 );
-//const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`),
 );
