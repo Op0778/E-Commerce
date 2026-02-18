a05b40a875f4c6b067d9270277d2326a8f04a9ff
Op0778 <prakashlkom5@gmail.com>
2026-02-18 21:02:06 +0530

    update ai

--- src/pages/url.js
+++ src/pages/url.js
@@ -1,4 +1,4 @@
-//const connectionUrl = "https://ecommerce-backend-b23p.onrender.com";
-const connectionUrl = "http://localhost:5000";
+const connectionUrl = "https://ecommerce-backend-b23p.onrender.com";
+// const connectionUrl = "http://localhost:5000";
 
 export default connectionUrl;
